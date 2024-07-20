f57cb3e8461dfe8c3253774f919b881f7e6bda58
Suraj Kumar <49224173+surajkumar85@users.noreply.github.com>
2024-07-20 07:05:40 -0700

    feat: added next UI

--- src/provider/provider.tsx
+++ src/provider/provider.tsx
@@ -1,7 +1,6 @@
 import { ReactNode } from "react";
+import { NextUIProvider } from "@nextui-org/react";
 
-export default function Provider({children}:{children:ReactNode}){
-    return <>
-    {children}
-    </>
+export default function Provider({ children }: { children: ReactNode }) {
+  return <NextUIProvider>{children}</NextUIProvider>;
 }
