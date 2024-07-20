a97851c258e99ccbe5c884d8ecbbe8ca8f6dc28d
Yashpal <yash38709@gmail.com>
2024-07-20 23:12:24 +0530

    sudo navbar created

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,3 +1,13 @@
 export default function Navbar() {
-  return <div>navbar</div>;
+  return (
+    <header className="h-14 bg-gray-200 flex justify-center items-center">
+      <div className="w-[95%] h-full bg-slate-50">
+        <div className="flex items-center justify-between px-4 h-full">
+          <div className="">Logo</div>
+          <div className="">List</div>
+          <div className="">Contact</div>
+        </div>
+      </div>
+    </header>
+  );
 }
