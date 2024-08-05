376b240e1d56c572b7c021b18634b4f2ed76aff3
Shivam Mudgil <49224235+shivam36363636@users.noreply.github.com>
2024-08-06 00:24:53 +0530

    Worked on "HOW DO WE HELP","WHY CHOOSE US" and "Enquiry Now" Sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import Enquiry from "@/components/enquiry";
+import Howhelp from "@/components/how-we-help";
 import Ourservices from "@/components/ourservices/our-services";
 import Section2card from "@/components/section-card/section2-card";
 import Section3card from "@/components/section-card/section3-card";
 import Section4card from "@/components/section-card/section4-card";
 import WelcomeSection from "@/components/section-card/welcome-section";
+import Whychooseus from "@/components/whyus/why-choose-us";
 
 export default function Home() {
   return (
@@ -12,6 +15,9 @@ export default function Home() {
       <Section3card />
       <Section4card />
       <Ourservices />
+      <Howhelp />
+      <Whychooseus />
+      <Enquiry />
     </div>
   );
 }
