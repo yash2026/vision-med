3cd9cbf125f059b6e3dd7d35411999f4ac87ed62
Suraj Kumar <49224173+surajkumar85@users.noreply.github.com>
2024-08-06 00:29:05 +0530

    fix: subfooter fix

--- src/components/sub-footer.tsx
+++ src/components/sub-footer.tsx
@@ -4,18 +4,19 @@ import WidthWrapper from "./width-wrapper";
 
 function Subfooter() {
   const subfooterKeys = Object.keys(subfooter);
-  console.log(subfooterKeys);
   return (
     <WidthWrapper className="mt-10 max-[1250px]:px-10 max-[600px]:px-4">
       <div className="grid grid-cols-4 py-8 max-[1250px]:gap-8 max-[800px]:grid-cols-3 max-[638px]:grid-cols-2 max-[638px]:text-xs max-[600px]:gap-4">
-        {subfooterKeys.map((key, index) => (
+        {subfooterKeys.map((footerKey, index) => (
           <div key={index} className="flex flex-col gap-3">
-            <h3 className="text-xl font-bold max-[500px]:text-lg">{key}</h3>
+            <h3 className="text-xl font-bold max-[500px]:text-lg">
+              {footerKey}
+            </h3>
             <div className="flex flex-col gap-3">
               {(subfooter as Record<string, { name: string; path: string }[]>)[
-                key
-              ]?.map((link) => (
-                <Link key={link.name} href={link.path} children={link.name} />
+                footerKey
+              ]?.map((link, index) => (
+                <Link key={index} href={link.path} children={link.name} />
               ))}
             </div>
           </div>
