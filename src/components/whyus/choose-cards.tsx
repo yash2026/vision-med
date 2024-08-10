fe38a32044874600e79def8921b25b46bae26666
Yashpal <yash38709@gmail.com>
2024-08-11 00:00:44 +0530

    Layout improved and pdf added

--- src/components/whyus/choose-cards.tsx
+++ src/components/whyus/choose-cards.tsx
@@ -4,27 +4,21 @@ import { WhyDataType } from "./why-choose-us";
 
 function Choosecards({ description, digit, icon, title }: WhyDataType) {
   return (
-    <div className="hover:-translate-y-2 transition-all">
-      <Card className="hover:border-b-5 hover:border-b-[#a97c25]">
-        <CardBody>
-          <div className="flex flex-col text-sm p-4 mb-0 ">
-            <div className="flex justify-between mb-5 items-center pt-4">
-              <img
-                src={icon}
-                className="h-16 w-16 justify-self-center text-[#2F4688]"
-              />
-              <p className="text-[70px] font-bold text-black/25 pr-2">
-                0{digit}
-              </p>
-            </div>
-            <p className="font-bold mb-2 mt-3 text-lg text-[#2f4688]">
-              {title}
-            </p>
-            <p className="leading-7">{description}</p>
+    <Card className="hover:-translate-y-2 transition-all hover:border-b-5 hover:border-b-[#a97c25] flex-grow">
+      <CardBody>
+        <div className="flex flex-col text-sm p-4 mb-0 ">
+          <div className="flex justify-between mb-5 items-center pt-4">
+            <img
+              src={icon}
+              className="h-16 w-16 justify-self-center text-[#2F4688]"
+            />
+            <p className="text-[70px] font-bold text-black/25 pr-2">0{digit}</p>
           </div>
-        </CardBody>
-      </Card>
-    </div>
+          <p className="font-bold mb-2 mt-3 text-lg text-[#2f4688]">{title}</p>
+          <p className="leading-7">{description}</p>
+        </div>
+      </CardBody>
+    </Card>
   );
 }
 
