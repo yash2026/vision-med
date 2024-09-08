96e0b7c2ce9c53aee26a06a472b352caa9670a06
Suraj Kumar <145673303+rsys-suraj-kumar@users.noreply.github.com>
2024-09-08 20:29:35 +0530

    added pages

--- src/app/(footer)/privacy-policy/page.tsx
+++ src/app/(footer)/privacy-policy/page.tsx
@@ -1,3 +1,34 @@
+import { BreadcrumbsBuilder, PageHeading } from "@/components/common";
+import { Paragraph } from "@/components/section-card/section-card";
+import WidthWrapper from "@/components/width-wrapper";
+
+const breadsData = [
+  {
+    title: "Home",
+    link: "/",
+  },
+  {
+    title: "Privacy Policy",
+    link: "/privacy-policy",
+  },
+];
+
 export default function page() {
-  return <div>Privacy and policy</div>;
+  return (
+    <WidthWrapper>
+      <PageHeading className="mt-10">Privacy Policy</PageHeading>
+      <BreadcrumbsBuilder breads={breadsData} />
+      <Paragraph className="mt-10">
+        Vision medicos is committed to protecting your privacy. We collect and
+        use your personal information only for the purpose of providing you with
+        the services you have requested. We may use your personal information to
+        communicate with you about our services and to improve our offerings. We
+        do not share your personal information with any third party unless
+        required by law. We take all necessary precautions to safeguard your
+        personal information and ensure its confidentiality. By using our
+        services, you agree to the terms of our privacy policy. If you have any
+        questions or concerns about our privacy practices, please contact us..
+      </Paragraph>
+    </WidthWrapper>
+  );
 }
--- src/app/(footer)/terms-and-conditions/page.tsx
+++ src/app/(footer)/terms-and-conditions/page.tsx
@@ -1,3 +1,34 @@
+import { BreadcrumbsBuilder, PageHeading } from "@/components/common";
+import { Paragraph } from "@/components/section-card/section-card";
+import WidthWrapper from "@/components/width-wrapper";
+
+const breadsData = [
+  {
+    title: "Home",
+    link: "/",
+  },
+  {
+    title: "Terms and conditions",
+    link: "/terms-and-conditions",
+  },
+];
+
 export default function page() {
-  return <div>Terms and conditions</div>;
+  return (
+    <WidthWrapper>
+      <PageHeading className="mt-10">Terms and conditions</PageHeading>
+      <BreadcrumbsBuilder breads={breadsData} />
+      <Paragraph className="mt-10">
+        The terms and conditions of Vision medicos govern the use of our website
+        and the services we offer. By using our website and services, you agree
+        to be bound by these terms and conditions. We reserve the right to
+        modify these terms and conditions at any time without notice. It is your
+        responsibility to review these terms and conditions regularly to stay
+        updated. The content on our website is for informational purposes only
+        and does not constitute professional advice. We do not guarantee the
+        accuracy, completeness, or reliability of the content. Any reliance on
+        the content is at your own risk.
+      </Paragraph>
+    </WidthWrapper>
+  );
 }
