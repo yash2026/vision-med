56e83345ef92225bde07d6cd8bdd342bb8d4baa6
Yashpal <yash38709@gmail.com>
2024-09-09 01:26:17 +0530

    Modified

--- src/components/address-footer.tsx
+++ src/components/address-footer.tsx
@@ -1,5 +1,12 @@
 import Link from "next/link";
 import WidthWrapper from "./width-wrapper";
+import {
+  FaEnvelope,
+  FaMapMarkerAlt,
+  FaPhoneAlt,
+  FaWhatsapp,
+} from "react-icons/fa";
+import { FaSquareWhatsapp } from "react-icons/fa6";
 
 export default function AddressFooter() {
   return (
@@ -7,6 +14,7 @@ export default function AddressFooter() {
       <div>
         <div>
           <span className="text-sm font-semibold text-black/50">
+            <FaMapMarkerAlt className="text-black/50 mr-2 inline-block" />
             Office No. 26, Ground Floor, Pacific City Centre, Near Jat Bhawan,
             Rohtak (Hr.)
           </span>
@@ -15,15 +23,22 @@ export default function AddressFooter() {
           className="text-sm font-semibold text-black/50"
           href={"mailto:visionmedicos333@gmail.com"}
         >
+          <FaEnvelope className="text-black/50 mr-2 inline-block" />
           visionmedicos333@gmail.com
         </Link>
       </div>
       <div className="flex flex-col text-sm font-semibold whitespace-nowrap text-black/50">
         <Link className="whitespace-nowrap" href={"tel:+91-70117-62602"}>
+          <FaPhoneAlt className="text-black/50 mr-2 inline-block" />
           +91-70117-62602
         </Link>
         <Link className="whitespace-nowrap" href={"tel:+91-95881-17017"}>
+          <FaPhoneAlt className="text-black/50 mr-2 inline-block" />
           +91-95881-17017
+        </Link>
+        <Link className="whitespace-nowrap" href={"https://wa.me/998902248111"}>
+          <FaSquareWhatsapp className="text-[#45c653] mr-2 inline-block" />
+          +998 90 224 81 11
         </Link>
       </div>
     </WidthWrapper>
